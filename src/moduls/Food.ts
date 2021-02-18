cfbc2792833f15fea4dd712623b7c88c0ee20fbd
lincmin <lincm69696@qq.com>
2021-02-18 11:34:07 +0800

    食物类

---
+++ src/moduls/Food.ts
@@ -0,0 +1,36 @@
+//定义食物类Food
+class Food{
+  //定义一个属性表示食物所对应的元素
+  element:HTMLElement
+  constructor(){
+    //获取页面中的 food 元素并将其赋值给 element
+    this.element = document.getElementById('food')!
+  }
+
+  //定义一个获取食物 x 轴坐标的方法
+  get X(){
+    return this.element.offsetLeft
+  }
+
+  //定义一个获取食物 Y 轴坐标的方法
+  get Y(){
+    return this.element.offsetTop
+  }
+
+  //修改食物的位置
+  change(){
+    //生成随机位置
+    //食物位置最小 0,最大 290
+    //蛇移动一次就是一格,一格的大小是 10,所以食物坐标必须是 10 的倍数
+    //向上取整
+    let top= Math.round( Math.random()*29)*10
+    let left= Math.round( Math.random()*29)*10
+    this.element.style.left = left+'px'
+    this.element.style.top = top+'px'
+  }
+}
+
+// const food = new Food()
+// food.change()
+
+export default Food
