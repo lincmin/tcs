83cda45e6176ea7bf998893d6333b91ca19cb1f9
lincmin <lincm69696@qq.com>
2021-02-27 17:46:23 +0800

    判断蛇撞墙

--- src/moduls/Snake.ts
+++ src/moduls/Snake.ts
@@ -20,9 +20,26 @@ class Snake{
   }
   //设置蛇头坐标
   set X(value:number){
+    //新旧值相同,则直接返回不修改值
+    if(this.X == value){
+      return
+    }
+    //X值的合法范围0-290
+    if(value < 0 || value > 290){
+      //蛇撞墙,抛出异常
+      throw new Error('蛇撞墙了')
+    }
     this.head.style.left = value + 'px'
   }
   set Y(value:number){
+    if(this.Y == value){
+      return
+    }
+    //Y值的合法范围0-290
+    if(value < 0 || value > 290){
+      //蛇撞墙
+      throw new Error('蛇撞墙了')
+    }
     this.head.style.top = value + 'px'
   }
   //设置蛇增加身体的方法
